9dbb5d152906d5e6302e93ebe049b062a3c2d42f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-11-26 18:37:01 +0100

    Split arch::config

--- modules/arch/manifests/config/xorg.pp
+++ modules/arch/manifests/config/xorg.pp
@@ -1,4 +1,4 @@
-class arch::xorg {
+class arch::config::xorg {
   exec { 'localectl set-x11-keymap fr':
     creates => '/etc/X11/xorg.conf.d/00-keyboard.conf',
     path    => $::path,
--- modules/arch/manifests/init.pp
+++ modules/arch/manifests/init.pp
@@ -2,7 +2,4 @@ class arch {
   class { 'arch::install': } ->
   class { 'arch::config': } ->
   Class['arch']
-
-  class { 'arch::xorg': }
 }
-
