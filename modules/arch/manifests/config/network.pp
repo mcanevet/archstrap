6a1c092fc6404992f495557e51ed9a57c3e5fcfa
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-11-29 18:46:24 +0100

    Use NetworkManager instead of systemd-networkd

--- modules/arch/manifests/config/network.pp
+++ modules/arch/manifests/config/network.pp
@@ -1,21 +1,38 @@
-class arch::config::network {
-  file { '/etc/systemd/network/MyDhcp.network':
-    ensure  => file,
-    content => "[Match]\nName=en*\n\n[Network]\nDHCP=v4\n",
-    notify  => Service['systemd-networkd'],
-  }
-  service { 'systemd-networkd':
-    ensure => running,
-    enable => true,
-  }
+class arch::config::network(
+  $provider = 'networkmanager',
+) {
+  case $provider {
+    'systemd': {
+      file { '/etc/systemd/network/MyDhcp.network':
+        ensure  => file,
+        content => "[Match]\nName=en*\n\n[Network]\nDHCP=v4\n",
+        notify  => Service['systemd-networkd'],
+      }
+      service { 'systemd-networkd':
+        ensure => running,
+        enable => true,
+      }
 
-  service { 'systemd-resolved':
-    ensure => running,
-    enable => true,
-  }
-  ->
-  file { '/etc/resolv.conf':
-    ensure => link,
-    target => '/run/systemd/resolve/resolv.conf',
+      service { 'systemd-resolved':
+        ensure => running,
+        enable => true,
+      }
+      ->
+      file { '/etc/resolv.conf':
+        ensure => link,
+        target => '/run/systemd/resolve/resolv.conf',
+      }
+    }
+    'networkmanager': {
+      package { 'networkmanager':
+        ensure => 'present',
+        before => Service['NetworkManager'],
+      }
+      ~>
+      service { 'NetworkManager':
+        ensure => 'running',
+        enable => true,
+      }
+    }
   }
 }
